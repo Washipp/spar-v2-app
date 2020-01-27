467b9f7e639b7d8068c0610b262dce87eb690574
SilasMeier <silas.meier@bluewin.ch>
2020-01-27 21:50:05 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'spar-v2-app';
+}
